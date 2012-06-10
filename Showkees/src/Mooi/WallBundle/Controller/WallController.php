543e8847ea1785d05b780ef01e4083a738950644
henkiexxx4@gmail.com <henkiexxx4@gmail.com@083f651e-4df7-c178-edc7-e9bf8a1ba033>
2012-06-10 21:44:38 +0000

    wall geupdate met een edit, delete en add controller

--- Showkees/src/Mooi/WallBundle/Controller/WallController.php
+++ Showkees/src/Mooi/WallBundle/Controller/WallController.php
@@ -16,44 +16,163 @@ class WallController extends Controller
     public function indexAction($id)
     {
         
+        $user = $this->get('security.context')->getToken()->getUser();
+        $wallOwner = $this->getDoctrine()
+            ->getRepository('MooiUserBundle:User')
+            ->find($id);
+        
+        return $this->render('MooiWallBundle:Wall:index.html.twig', array(
+                'id'                => $id,
+                'wallOwner'         => $wallOwner,
+                'user'              => $user
+        ));
+   
+    }
+    
+    public function addAction($id)
+    {
+        
         $request = $this->getRequest();
         $user = $this->get('security.context')->getToken()->getUser();
         $wallOwner = $this->getDoctrine()
             ->getRepository('MooiUserBundle:User')
             ->find($id);
         
-        /*$rp = $this->getDoctrine()
-                   ->getRepository('MooiUserBundle:User');
-        $query = $rp->createQueryBuilder('u')
-            ->where('u.id = :id')
-            ->setParameter('id', $id)*/
-            
-        /*$query = 'SELECT u 
-                        FROM MooiUserBundle:User u 
-                        INNER JOIN MooiWallBundle:Post p  
-                        WHERE u.id = :id
-                        ORDER BY p.time DESC';
-            $em = $this->getDoctrine()->getEntityManager();
-            $result = $em->createQuery($query)
-                        ->setParameter('id', $id);
-            $wallOwner = $result->getResult();*/
-        
-        $roleId = $user->getRole()->getId();
         
         //set new post object and create form
         $newPost = new Post();
         $form = $this->createForm(new WallPostType(), $newPost);
-       
-            
-            return $this->render('MooiWallBundle:Wall:index.html.twig', array(
+        
+        if ($request->getMethod() == 'POST') 
+        {
+
+            $form->bindRequest($request);   
+
+            if ($form->isValid()) 
+            {
+
+                //set post data
+                $newPost->setTime(new \DateTime);
+                $newPost->setSender($user);
+                $newPost->setWallOwner($wallOwner);
+
+                // Save the Post to the database
+                $em = $this->getDoctrine()->getEntityManager();
+                $em->persist($newPost);
+                $em->flush();
+                
+                $this->get("session")->setFlash('notice', 'De post is toegevoegd.');
+                
+                return $this->redirect($this->generateUrl('MooiWallBundle_WallIndex', array(
+                    'id'  => $id
+                )));
+
+            }
+
+        }
+        
+        return $this->render('MooiWallBundle:Wall:add.html.twig', array(
                     'form'              => $form->createView(),
                     'id'                => $id,
                     'wallOwner'         => $wallOwner
-            ));
-
-      
-
+        ));
         
+    }
+    
+    public function editAction($postId)
+    {
+        
+        $request = $this->getRequest();
+        $post = $this->getDoctrine()
+            ->getRepository('MooiWallBundle:Post')
+            ->find($postId);
+        
+        $wallOwnerId = $post->getWallOwner()->getId();
+        
+        if($post == null)
+        {
+            
+            throw $this->createNotFoundException("De post kon niet gevonden worden");
+            
+        }
+        else
+        {
+            
+            $form = $this->createForm(new WallPostType(), $post);
+        
+            if ($request->getMethod() == 'POST') 
+            {
+
+                $form->bindRequest($request);   
+
+                if ($form->isValid()) 
+                {
+
+                    //set post data
+                    $post->setTime(new \DateTime);
+
+                    // Save the Post to the database
+                    $em = $this->getDoctrine()->getEntityManager();
+                    $em->persist($post);
+                    $em->flush();
+
+                    $this->get("session")->setFlash('notice', 'De post is gewijzigd.');
+
+                    return $this->redirect($this->generateUrl('MooiWallBundle_WallIndex', array(
+                        'id'  => $wallOwnerId
+                    )));
+
+                }
+
+            }
+            else
+            {
+                
+                return $this->render('MooiWallBundle:Wall:edit.html.twig', array(
+                    'form'              => $form->createView(),
+                    'post'            => $post
+                ));
+                
+            }
+            
+            
+        }
+        
+    }
+    
+    public function deleteAction($postId)
+    {
+        
+        $post = $this->getDoctrine()
+            ->getRepository('MooiWallBundle:Post')
+            ->find($postId);
+        
+        $user = $this->get('security.context')->getToken()->getUser();
+        $wallOwnerId = $post->getWallOwner()->getId();
+        
+        if($post != null)
+        {
+            
+            $entityManager = $this->getDoctrine()->getEntityManager();
+            $entityManager->remove($post);
+            $entityManager->flush();
+
+            $this->get("session")->setFlash('notice', 'De post is verwijderd.');
+            
+            return $this->redirect($this->generateUrl('MooiWallBundle_WallIndex', array(
+                'id'  => $wallOwnerId
+            )));
+            
+        }
+        else
+        {
+            
+            return $this->redirect($this->generateUrl('MooiWallBundle_WallIndex', array(
+                'id'  => $user->getId()
+            )));
+            
+        }
+         
     }
     
 
